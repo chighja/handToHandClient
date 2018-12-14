c372a96f417e6e92546c827cd5ae30439e15ed36
chighja <chighja@gmail.com>
2018-12-13 19:01:16 -0500

    updated API_BASE_URL address

--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 export const API_BASE_URL =
-  process.env.REACT_APP_API_BASE_URL || 'http://localhost:5005';
+  process.env.REACT_APP_API_BASE_URL ||
+  'https://stormy-falls-89261.herokuapp.com/';
