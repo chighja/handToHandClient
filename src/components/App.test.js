d22db7e607fe37f8e7b8517de9494dda927603f0
chighja <chighja@gmail.com>
2018-12-20 19:32:27 -0500

    finished tests

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -4,16 +4,7 @@ import App from './app';
 import Header from './Header';
 
 describe('<App />', () => {
-  let wrapper;
-  beforeEach(() => (wrapper = shallow(<App />)));
-
-  it('renders without crashing', () => {
-    shallow(<App />);
-  });
-
-  it('should render a <div />', () => {
-    expect(wrapper.find('div').length).toEqual(1);
-  });
+  const wrapper = shallow(<App />);
 
   it('should render the <Header /> component', () => {
     expect(wrapper.containsMatchingElement(<Header />)).toEqual(true);
--- src/components/LandingView.test.js
+++ src/components/LandingView.test.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
+import { Link } from 'react-router-dom';
+import { MemoryRouter } from 'react-router';
 import LandingView from './LandingView';
+import Matches from './Matches';
 
 describe('<LandingView />', () => {
-  const btnClick = jest.fn();
-  const wrapper = shallow(<LandingView />);
+  const getDefault = () => shallow(<LandingView />);
 
-  it('should fire btnClick function when clicked', () => {
-    wrapper.find('form').simulate('submit');
-
-    expect(btnClick).toHaveBeenCalled();
+  it('should include link to <Matches /> endpoint', () => {
+    const wrapper = getDefault();
+    expect(wrapper.find(Link).props().to).toBe('/matches');
   });
 });
